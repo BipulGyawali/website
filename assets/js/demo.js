63dab45835fd075a53cedacc343770c625fe7c98
bipulgyawali <85114433+bipulgyawali@users.noreply.github.com>
2021-11-28 00:31:37 -0500

    Update demo.js

--- assets/js/demo.js
+++ assets/js/demo.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     particleground(document.getElementById('particles'), {
         dotColor: '#bdb75c',
-        lineColor: '#5cbdaa'
+        lineColor: '#bdb75c'
     });
     var intro = document.getElementById('intro');
     intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
